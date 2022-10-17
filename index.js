d61b8a42ceed51334c82f64f720d164cd31f2e41
qaiik <82181231+qaiik@users.noreply.github.com>
2022-10-16 21:54:13 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+class PrUrl {
+  constructor(base) {
+    this.url = base
+  }
+
+  navigates(by) {
+    this.url = new URL(by, this.url).toString()
+    return this.url
+  }
+
+  navigate(by) {
+    this.url = new URL(by, this.url).toString()
+    return this
+  }
+
+  get() {
+    return this.url
+  }
+  isAbs() {
+    try {
+      new URL(this.url)
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+}
+
+class ProxyApplier {
+  constructor(base) {
+    this.url = base;
+  }
+  
+  MainUrl(d) {
+    let pr = new PrUrl(this.url)
+    if (!pr.isAbs()) {
+      this.url = new PrUrl(this.url).navigates(d)
+    }
+  }
+  
+  Proxy(proxyurl) {
+    this.url = new PrUrl(proxyurl).navigates(proxyurl)
+  }
+
+}
